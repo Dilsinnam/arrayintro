a71722b2b66bf95f999ea7ee6af7caf715dfabef
Dilsin Amirtharajah <KillerzDaggers@Outlook.com>
2022-09-29 12:51:53 -0400

    Initial Commit

---
+++ arrays.js
@@ -0,0 +1,23 @@
+let x = 5;
+const y = 10;
+
+/*let studentGrade1 = 92;
+let studentGrade2 = 85;
+let studentGrade3 = 78;
+
+const studentGrades = [92, 85, 78, 97];
+const names = ["Ronaldo", "Messi", "Ozil", "Martial"]
+const booleans = [true, false, true, true]
+const mixed = [0, "Ronaldo", true, false, [4,5,6]]
+mixed[4] = 0;
+console.log(mixed[4]);
+
+studentGrades.push(55);
+console.log(studentGrades);*/
+
+const studentGrades = [92, 84, 76, 99, 62, 67, 79, 88];
+
+for (let i = 0; i < studentGrades.length; i++) {
+    console.log(studentGrades[i])
+}
+console.log(studentGrades.length);
